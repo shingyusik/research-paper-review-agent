a73b42177e0c3f3194a053b3036a868a78f903a3
wjchoi <wjchoi@e8ight.co.kr>
2025-12-22 13:44:47 +0900

    Add GUI

--- research_paper_review_agent/__init__.py
+++ research_paper_review_agent/__init__.py
@@ -1,9 +1,11 @@
 from .interface.cli import run_cli
+from .interface.gui import run_gui
 from .core.agent import build_agent, run_agent
 from .core.state import State, BasicInfo
 
 __all__ = [
     "run_cli",
+    "run_gui",
     "build_agent",
     "run_agent",
     "State",
